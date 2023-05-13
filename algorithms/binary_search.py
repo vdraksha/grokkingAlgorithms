3a714ebe0df97d8eda2e83cb57d0110496dc75ad
vdraksha <vdraksha@yandex.ru>
2023-05-13 20:05:23 +0500

    Добавлен binary_search_recursive в binary_search.py в algorithms.

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -23,5 +23,21 @@ def binary_search(array, desired_item):
     return None
 
 
+@timer
+def binary_search_recursive(array, desired_item, left=0, right=None):
+    if not right:
+        right = len(array) - 1
+    if left > right:
+        return False
+
+    position = (left + right) // 2
+    if desired_item == array[position]:
+        return position
+    elif array[position] > desired_item:
+        return binary_search_recursive(array, desired_item, left, position-1)
+    return binary_search_recursive(array, desired_item, position+1, right)
+
+
 if __name__ == "__main__":
     pass
+
