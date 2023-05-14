881520701b49378cf99c518ec21dd1c983401a2f
vdraksha <vdraksha@yandex.ru>
2023-05-14 21:45:51 +0500

    Добавлен selection_sort.py в algorithms.

---
+++ algorithms/selection_sort.py
@@ -0,0 +1,38 @@
+"""Сортировка выбором - алгоритм сортировки со сложностью О(n^2)"""
+from support.decorators import timer
+
+
+@timer
+def selection_sort(array):
+    for i in range(len(array)):
+        min_index = i
+        for j in range(i + 1, len(array)):
+            if array[j] < array[min_index]:
+                min_index = j
+        if min_index != i:
+            array[i], array[min_index] = array[min_index], array[i]
+    return array
+
+
+def find_min(array, i, min_index):
+    if i == len(array):
+        return min_index
+    if array[i] < array[min_index]:
+        return find_min(array, i+1, i)
+    return find_min(array, i+1, min_index)
+
+
+@timer
+def selection_sort_recursive(array, i=0):
+    if i == len(array) - 1:
+        return array
+
+    min_index = find_min(array, i+1, i)
+
+    if min_index != i:
+        array[i], array[min_index] = array[min_index], array[i]
+    return selection_sort_recursive(array, i+1)
+
+
+if __name__ == '__main__':
+    pass
